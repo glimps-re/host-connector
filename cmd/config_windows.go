d405dc2a6fede5972da8ad5c0a7304487f98f2e8
glimps-jbo <glimps-jbo@glimps-re>
2024-08-14 18:50:04 +0200

    fix: use message box for error logs

--- cmd/config_windows.go
+++ cmd/config_windows.go
@@ -4,9 +4,13 @@
 package cmd
 
 import (
+	"bytes"
+	"context"
 	"log/slog"
 	"os"
 	"path/filepath"
+
+	"github.com/gonutz/wui/v2"
 )
 
 var (
@@ -29,4 +33,44 @@ func getConfigFile() (config string) {
 		}
 	}
 	return
+}
+
+type GuiErrorLog struct {
+	h slog.Handler
+}
+
+func (h *GuiErrorLog) Enabled(ctx context.Context, level slog.Level) bool {
+	return h.h.Enabled(ctx, level)
+}
+
+func (h *GuiErrorLog) Handle(ctx context.Context, r slog.Record) error {
+	if r.Level == slog.LevelError {
+		buffer := bytes.Buffer{}
+		log := slog.NewJSONHandler(&buffer, nil)
+		log.Handle(ctx, r)
+		wui.MessageBoxError("GMhost Error", buffer.String())
+	}
+	return h.h.Handle(ctx, r)
+}
+
+func (h *GuiErrorLog) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return h.h.WithAttrs(attrs)
+}
+
+func (h *GuiErrorLog) WithGroup(name string) slog.Handler {
+	return h.h.WithGroup(name)
+}
+
+func init() {
+	// dirty hack to check if we run as windowgui executable
+	defer func() {
+		if r := recover(); r != nil {
+			Logger = slog.New(&GuiErrorLog{h: Logger.Handler()})
+		}
+	}()
+	// must panic with windowsgui exe
+	info, _ := os.Stderr.Stat()
+	if info == nil {
+		Logger = slog.New(&GuiErrorLog{h: Logger.Handler()})
+	}
 }
